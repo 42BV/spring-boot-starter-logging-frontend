b653f3fe7bb7ad57252da3bba73e24ab6b86a1e2
Jeroen van Schagen <jeroen.van.schagen@42.nl>
2019-05-03 14:29:59 +0200

    Improved tests

--- src/test/java/nl/_42/boot/logging/frontend/FrontendLoggerControllerTest.java
+++ src/test/java/nl/_42/boot/logging/frontend/FrontendLoggerControllerTest.java
@@ -1,8 +1,17 @@
 package nl._42.boot.logging.frontend;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.LoggerFactory;
 
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -10,16 +19,57 @@ public class FrontendLoggerControllerTest extends AbstractWebIntegrationTest {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    private Logger frontEndLogger;
+    private ListAppender<ILoggingEvent> frontEndLoggerAppender;
+
+    @Before
+    public void setUp() {
+        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+        frontEndLogger = context.getLogger(LoggerFactory.getLogger(FrontendLogger.class).getName());
+
+        frontEndLoggerAppender = new ListAppender<>();
+        frontEndLoggerAppender.start();
+        frontEndLogger.addAppender(frontEndLoggerAppender);
+
+        frontEndLogger.setLevel(Level.ALL);
+    }
+
     @Test
     public void log_error() throws Exception {
-        FrontendError error = new FrontendError();
-        error.setMessage("Something went wrong");
-        error.setStack("abc");
-        error.setUrl("http://localhost");
-        error.setUserAgent("Chrome");
+        FrontendError frontendError = new FrontendError();
+        frontendError.setMessage("Big error in little China");
+        frontendError.setUrl("https://www.42.nl");
+        frontendError.setStack("Le big trace du stack");
+        frontendError.setUserAgent("Chrome on mac os");
 
-        webClient.perform(post("/log/error").content(objectMapper.writeValueAsString(error)))
+        webClient.perform(post("/log/error").content(objectMapper.writeValueAsString(frontendError)))
           .andExpect(status().isOk());
+
+        assertEquals(1, frontEndLoggerAppender.list.size());
+
+        ILoggingEvent loggedEvent = frontEndLoggerAppender.list.get(0);
+        assertEquals(Level.ERROR, loggedEvent.getLevel());
+
+        String expectedMessage = "Big error in little China\n"
+          + "URL: https://www.42.nl\n"
+          + "userAgent: Chrome on mac os\n"
+          + "Stack: Le big trace du stack";
+
+        assertEquals(expectedMessage, loggedEvent.getMessage());
+    }
+
+    @Test
+    public void log_error_fail() throws Exception {
+        FrontendError frontendError = new FrontendError();
+        frontendError.setMessage("Big error in little China");
+        frontendError.setUrl("https://www.42.nl");
+        frontendError.setStack("Le big trace du stack");
+        frontendError.setUserAgent(StringUtils.repeat("a", 129));
+
+        webClient.perform(post("/log/error").content(objectMapper.writeValueAsString(frontendError)))
+          .andExpect(status().is4xxClientError());
+
+        assertEquals(0, frontEndLoggerAppender.list.size());
     }
 
 }
